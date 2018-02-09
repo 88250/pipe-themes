874f4a6fdc99d591e1876e6b1e5fde49a7e31cb8
Vanessa <lly219@gmail.com>
2018-02-09 13:06:04 +0800

    :art: new theme

--- x/9IPHP/js/article.js
+++ x/9IPHP/js/article.js
@@ -15,14 +15,56 @@ const Article = {
    * @description 页面初始化
    */
   init: () => {
+    $('#articleCommentBtn').click(function () {
+      const $this = $(this)
+      ShowEditor($this.data('title'), $this.data('id'))
+    })
+
+    if ($('#toc').length === 1) {
+      InitToc('toc', 'articleContent')
+      $('.side__toc').width($('.side').width()).css('max-height', ($(window).height() - 170) + 'px')
+
+      $(window).scroll(function () {
+        if ($(window).scrollTop() > 75) {
+          $('.side__toc').addClass('side__toc--fix')
+        } else {
+          $('.side__toc').removeClass('side__toc--fix')
+        }
+      })
+
+      $('#hideToc').click(function () {
+        $(this).hide()
+        $('#showToc').show()
+        $('.side__toc').animate({
+          'margin-top': '300px'
+        }, 300, function () {
+          $(this).hide()
+          $('.side > .fn-none').show()
+        })
+      })
+
+      $('#showToc').click(function () {
+        $(this).hide()
+        $('#hideToc').show()
+        $('.side__toc').show().animate({
+          'margin-top': '0'
+        }, 100)
+        $('.side > .fn-none').hide()
+      })
+
+      if ($(window).width() < 768) {
+        $('#hideToc').click()
+      }
+    }
+
     InitComment()
     InitHljs()
 
     Article._share();
   },
   _share: () => {
-    const $this = $('.article__share')
-    const $qrCode = $this.find('.article__code')
+    const $this = $('.share__btns')
+    const $qrCode = $this.find('.share__code')
     const shareURL = $qrCode.data('url')
     const avatarURL = $qrCode.data('avatar')
     const title = encodeURIComponent($qrCode.data('title') + ' - ' + $qrCode.data('blogtitle')),
@@ -36,7 +78,7 @@ const Article = {
     urls.google = 'https://plus.google.com/share?url=' + url
     urls.twitter = 'https://twitter.com/intent/tweet?status=' + title + ' ' + url
 
-    $this.find('.article__share-btn').click(function () {
+    $this.find('.share__btn').click(function () {
       const key = $(this).data('type')
 
       if (!key) {
